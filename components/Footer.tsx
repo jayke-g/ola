393e064bc62c31b0349b5de2a53f96a4c0451de0
Jayke <137838888+jayke-g@users.noreply.github.com>
2023-11-24 23:03:41 +1100

    update download resume footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,12 +7,16 @@ export default function Footer() {
         <div className="pb-4 flex flex-row justify-between">
           <div className="flex flex-col-reverse hover:cursor-pointer">
             <Link href="/">
-              <img src="/logos/ola-footer.svg" alt="ola logo" className="w-20" />
+              <img
+                src="/logos/ola-footer.svg"
+                alt="ola logo"
+                className="w-20"
+              />
             </Link>
           </div>
           <div className="flex flex-col items-start">
             <div className="hover:underline">
-              <Link href="/Gubkina_designer_CV.pdf" target="_blank" download>
+              <Link href="/files/Gubkina_designer_CV.pdf" target="_blank">
                 RESUME
               </Link>
             </div>
